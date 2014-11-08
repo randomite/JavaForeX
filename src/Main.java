0d6e4e60d030f0a91ae89b0fa1aae402e05408c0
Ravin Sardal <ravin.sardal@gmail.com>
2014-11-07 22:39:02 -0800

    New token generation algorithm

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ public class Main {
 	public static void main(String[] args){
 		Token xPayToken = new Token();
 
-		String token = xPayToken.buildHash(args[0],args[1]);
+		xPayToken.buildHash(args[0],args[1]);
 		
-		System.out.println("xpay token is : " + token);
+		//System.out.println("xpay token is : " + token);
 	}
 
 }
